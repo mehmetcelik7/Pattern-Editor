34c124147c24eda2705496faa1f338cb26b0575a
mehmet <mehmet.cel.ik@hotmail.com>
2025-03-19 09:34:19 +0300

    Initial Commit

---
+++ Pattern Editor/Pattern_EditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pattern_EditorApp.swift
+//  Pattern Editor
+//
+//  Created by mehmet Çelik on 19.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Pattern_EditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
